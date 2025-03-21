96842d760d423d51a941f66bb16ff71b35fb18d0
ali munir <alimunirwaris1@gmail.com>
2025-03-21 05:45:07 +0500

    Remove .env.sample, update ESLint configuration for TypeScript and Drizzle, and update package dependencies including dotenv, drizzle-orm, drizzle-zod, and pg.

---
+++ src/db/schema/user.ts
@@ -0,0 +1,17 @@
+import {
+  integer,
+  pgTable,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+  id: serial("id").primaryKey().notNull(),
+  fullName: varchar("fullName", { length: 255 }).notNull(),
+  age: integer("age").notNull(),
+  password: varchar("password", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "string" }).notNull().defaultNow(),
+});
